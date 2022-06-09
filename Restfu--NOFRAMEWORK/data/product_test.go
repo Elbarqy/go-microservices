9ce9f9bdb6feaf323718290444283e5c2ae5c0b4
elbarqy <elbarky.dev@gmail.com>
2022-06-09 23:02:43 +0200

    changed the folder name

--- Restfu--NOFRAMEWORK/data/product_test.go
+++ Restfu--NOFRAMEWORK/data/product_test.go
@@ -1,3 +1,17 @@
+// Package classification of product API
+//
+// Documentation for Product API
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
+
 package data
 
 import "testing"
