eb6a651a52e03f2813c157bb79f1ebcfe839e4b5
elbarqy <elbarky.dev@gmail.com>
2022-06-10 19:13:08 +0200

    binding

--- Gin/main.go
+++ Gin/main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func main() {
 	router := gin.Default()
 	address := ":3000"
@@ -15,6 +20,19 @@ func main() {
 	v2 := v1.Group("/user")
 	v2.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Hello world")
+	})
+	v2.POST("/login", func(ctx *gin.Context) {
+		var body Login
+		if err := ctx.ShouldBindJSON(&body); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		if body.User != "manu" || body.Password != "123" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+		println("passed")
+		ctx.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 	v2.GET("/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
