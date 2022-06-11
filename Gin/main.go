bb3852b420e7acc9f732458bfbf7900da761d8d7
elbarqy <elbarky.dev@gmail.com>
2022-06-11 17:10:38 +0200

    custom binding validation

--- Gin/main.go
+++ Gin/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Login struct {
 	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
-	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required,password"`
 }
 
+func verifyPassword(fl validator.FieldLevel) bool {
+	var regex = regexp.MustCompile("\\w{8,}")
+	password := fl.Field().String()
+	return regex.MatchString(password)
+}
 func main() {
 	router := gin.Default()
 	address := ":3000"
@@ -21,6 +29,11 @@ func main() {
 	v2.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Hello world")
 	})
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("password", verifyPassword)
+	}
+
 	v2.POST("/login", func(ctx *gin.Context) {
 		var body Login
 		if err := ctx.ShouldBindJSON(&body); err != nil {
